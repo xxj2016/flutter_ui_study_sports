1226dbb8e0b9e7405cc3baca84e732efa0effc8e
Jet Xu <jet.xu@ccconceptcn.com>
2020-01-17 23:47:18 +0800

    添加产品图，以及TextUI

---
+++ lib/data.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+List<String> sizeNumlist = [
+  "7",
+  "8",
+  "9",
+  "10",
+];
+
+List<Color> colors = [
+  Color(0xFFF9362E),
+  Color(0xFF003CFF),
+  Color(0xFFFFB73A),
+  Color(0xFF3AFFFF),
+  Color(0xFF1AD12C),
+  Color(0xFFD66400),
+];
+
+String desc =
+    "Ad proident consectetur ex ullamco ad qui cillum consectetur. Eu laboris in amet laborum excepteur nisi nulla labore incididunt proident ea aliqua. Enim proident exercitation est incididunt dolor aute veniam nisi nulla sint id. Veniam sunt ex laboris fugiat exercitation tempor dolor aliquip proident deserunt dolor deserunt minim dolore. Eiusmod mollit sint Lorem amet dolore. Qui consequat excepteur proident ut excepteur ex velit laboris amet culpa anim id esse dolor."
+    "Exercitation excepteur sunt excepteur minim. Occaecat nostrud velit laborum cupidatat cupidatat labore laborum culpa ut cillum irure. Duis duis eiusmod magna incididunt elit. Enim nisi minim in in exercitation culpa sit. Fugiat eiusmod ad incididunt voluptate aute sint non voluptate aliqua id pariatur Lorem id nulla. Labore fugiat non culpa eiusmod. Voluptate id ipsum elit deserunt consectetur esse do ad incididunt."
+    "Sunt cupidatat id deserunt ipsum. Et aute labore esse nulla Lorem incididunt sint. Ipsum sint ex id in consectetur exercitation nisi sint deserunt excepteur. Quis fugiat occaecat dolore commodo culpa ea veniam consectetur et. Qui minim Lorem tempor labore voluptate ex est labore aute fugiat in labore do.";
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_study_sports/data.dart';
 import 'package:flutter_ui_study_sports/utils.dart';
+
+import 'utils.dart';
+import 'utils.dart';
+import 'utils.dart';
+import 'utils.dart';
+import 'utils.dart';
+import 'utils.dart';
+import 'utils.dart';
+import 'utils.dart';
+import 'utils.dart';
+import 'utils.dart';
+import 'utils.dart';
+import 'utils.dart';
+import 'utils.dart';
+import 'utils.dart';
+import 'utils.dart';
+import 'utils.dart';
+import 'utils.dart';
 
 void main() => runApp(MaterialApp(
       home: MyApp(),
@@ -30,18 +49,21 @@ class _MyAppState extends State<MyApp> {
           backgroundColor: Colors.transparent,
           elevation: 0.0,
           leading: IconButton(
-            icon: Icon(Icons.arrow_back, size: screenAwareSize(20.0, context),),
+            icon: Icon(
+              Icons.arrow_back,
+              size: screenAwareSize(20.0, context),
+            ),
             onPressed: () {},
           ),
           title: Text(
-            "Energy Cloud",
+            "Energy Cloud1",
             style: TextStyle(
               color: Colors.white,
               fontSize: screenAwareSize(18.0, context),
               // fontFamily: "",
             ),
           ),
-          centerTitle: true,  
+          centerTitle: true,
           actions: <Widget>[
             IconButton(
               icon: Icon(
@@ -49,11 +71,186 @@ class _MyAppState extends State<MyApp> {
                 size: screenAwareSize(20.0, context),
                 color: Colors.white,
               ),
-              onPressed: (){},
+              onPressed: () {},
             )
           ],
         ),
-        body: Container(),
+        body: Column(
+          children: <Widget>[
+            ProductScreenTopPart(),
+            ProductScreenBottomPart(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProductScreenTopPart extends StatefulWidget {
+  ProductScreenTopPart({Key key}) : super(key: key);
+
+  @override
+  _ProductScreenTopPartState createState() => _ProductScreenTopPartState();
+}
+
+class _ProductScreenTopPartState extends State<ProductScreenTopPart> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: screenAwareSize(245.0, context),
+      child: Stack(
+        children: <Widget>[
+          Container(
+            child: Image.asset(
+              "assets/adidas.png",
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 60.0, right: 35.0),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                width: screenAwareSize(50.0, context),
+                height: screenAwareSize(50.0, context),
+                decoration: BoxDecoration(
+                    color: Colors.black26.withOpacity(.6),
+                    shape: BoxShape.circle),
+                child: Image.asset("assets/360.png"),
+              ),
+            ),
+          ),
+          Positioned(
+              left: screenAwareSize(18.0, context),
+              bottom: screenAwareSize(15.0, context),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Rating",
+                    style: TextStyle(
+                        color: Color(0xff949598),
+                        fontSize: screenAwareSize(8.0, context),
+                        fontFamily: "Montserrat-SemiBold"),
+                  ),
+                  SizedBox(
+                    height: screenAwareSize(8.9, context),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      SizedBox(
+                        width: 8.0,
+                      ),
+                      Icon(
+                        Icons.star,
+                        color: Color(0xffffe600),
+                      ),
+                      SizedBox(
+                        width: screenAwareSize(5.0, context),
+                      ),
+                      Text(
+                        "4.5",
+                        style: TextStyle(color: Color(0xffffe600)),
+                      ),
+                      SizedBox(
+                        width: 8.0,
+                      ),
+                      Text(
+                        "(378 People)",
+                        style: TextStyle(color: Colors.white, fontSize: 14.0),
+                      )
+                    ],
+                  )
+                ],
+              ))
+        ],
+      ),
+    );
+  }
+}
+
+class ProductScreenBottomPart extends StatefulWidget {
+  ProductScreenBottomPart({Key key}) : super(key: key);
+
+  @override
+  _ProductScreenBottomPartState createState() =>
+      _ProductScreenBottomPartState();
+}
+
+class _ProductScreenBottomPartState extends State<ProductScreenBottomPart> {
+  bool isExpanded = false;
+
+  void _expand() {
+    setState(() {
+      isExpanded ? isExpanded = false : isExpanded = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(left: screenAwareSize(18.0, context)),
+            child: Text(
+              "Product Description",
+              style: TextStyle(
+                color: Color(0xff949598),
+                fontSize: screenAwareSize(10.0, context),
+                fontFamily: "Montserrat-SemiBold",
+              ),
+            ),
+          ),
+          SizedBox(
+            height: screenAwareSize(8.0, context),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              left: screenAwareSize(26.0, context),
+              right: screenAwareSize(18.0, context),
+            ),
+            child: AnimatedCrossFade(
+              firstChild: Text(
+                desc,
+                maxLines: 2,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: screenAwareSize(10.0, context),
+                  fontFamily: "Montserrar-Medium",
+                ),
+              ),
+              secondChild: Text(
+                desc,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: screenAwareSize(10.0, context),
+                  fontFamily: "Montserrat-Medium",
+                ),
+              ),
+              crossFadeState: isExpanded
+                  ? CrossFadeState.showSecond
+                  : CrossFadeState.showFirst,
+              duration: kThemeAnimationDuration,
+            ),
+          ),
+          Padding(
+              padding: EdgeInsets.only(
+                left: screenAwareSize(26.0, context),
+                bottom: screenAwareSize(10.0, context),
+              ),
+              child: GestureDetector(
+                onTap: _expand,
+                child: Text(
+                  isExpanded ? "less" : "more..",
+                  style: TextStyle(
+                      color: Color(0xfffb382f), fontWeight: FontWeight.w700),
+                ),
+              ))
+        ],
       ),
     );
   }
